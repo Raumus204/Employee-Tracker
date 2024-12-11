7e4e5a77f19a6a436435aa549958c103cbf3560b
Cory Melton <enacra204@gmail.com>
2024-12-11 13:24:57 -0600

    made menu

---
+++ main/dist/database/server.js
@@ -0,0 +1,9 @@
+"use strict";
+// import express from 'express'
+// import { connectToDb } from './connection.js';
+// await connectToDb();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
